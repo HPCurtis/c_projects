722feef7626a02602a9c7a4ac39a7c3adaeaef95
Harrison Curtis <harrisoncurtis50@gmail.com>
2024-08-01 16:50:55 +0100

    working programme

--- beginner/leapyear.cpp
+++ beginner/leapyear.cpp
@@ -6,7 +6,8 @@ using namespace std;
 
 // Function declarations (if any)
 int year_input();
-int leapYear(int year);
+bool leapYear(int year);
+int leapYearRes(bool res);
 
 int main()
 {
@@ -14,39 +15,69 @@ int main()
     //  Get user input
     int year = year_input();
     // cout << year;
-    leapYear(year);
+    bool res = leapYear(year);
+    leapYearRes(res);
     return 0;
 }
 
 int year_input()
 { 
     int year;
-    cout << "Enter a number that you want to know if its a leap year: ";
+    cout << "Enter a year that you want to know if its a leap year or not: ";
     
      while (true)
     {
         cin >> year;
-        if (!year)
+        // check the fail stat of cin stream which in this case is checck if the right variable typoe of int has been inputted.
+        if (cin.fail())
         {
-        cout << "Please enter an integer value for a year" << endl;
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        continue;
+            // reprompt for use input.
+            cout << "Please enter an integer value for a year" << endl;
+            cin.clear();
+            // clear the input bufer/
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            continue;
         }
         else break;
     }
 
     return year;
 }
-
-int leapYear(int year)
+// Function to calculate if a year is a leap year.
+bool leapYear(int year)
 {
     
+    // Check if year is divislibe by 4.
     if (year % 4 == 0)
     {
-        // Outputs 1 as that true.
-        cout << true;
-    } 
+    // Check if year is divislibe by 100.
+       if (year % 100 == 0)
+       {
+    // Check if year is divislibe by 400.  
+            if(year % 400 == 0)
+            {
+               return true;     
+         } else {
+                return false; // Divisible by 100 but not by 400: not a leap year
+            }
+        } else {
+            return true;  // Divisible by 4 but not by 100: leap year
+        }
+    } else {
+        return false; // Not divisible by 4: not a leap year
+    }
+}
+
+int leapYearRes(bool res)
+{
+    if (res == true)
+    {
+        cout << "The year you selected is a leap year.\n"; 
+    }
+    else if (res == false)
+    {
+        cout << "The year you selected is not a leap year.\n"; 
+    }
     
     return 0;
 }
